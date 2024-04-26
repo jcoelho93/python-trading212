fad87f15a3d45d2fc81cece8c7ea522606f77a9e
José Coelho <16445494+jcoelho93@users.noreply.github.com>
2024-04-26 17:37:21 +0100

    Add linting

--- python_trading212/__init__.py
+++ python_trading212/__init__.py
@@ -1,2 +1,2 @@
-from python_trading212.trading212 import Trading212
-from python_trading212.models import Position
+from python_trading212.trading212 import Trading212 # noqa
+from python_trading212.models import Position # noqa
